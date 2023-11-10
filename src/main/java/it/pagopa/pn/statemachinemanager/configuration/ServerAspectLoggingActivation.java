7ea6fd2b1fd1b33fc588442bb8c96d20f19fedd6
michelescara <michelescaramuzzino@gmail.com>
2023-11-10 16:10:31 +0100

    Revert "Introduced ServerAspectLoggingActivation."
    This reverts commit 9936fda098fcc610b4edaa0c1371e68da9a9cfca.

--- src/main/java/it/pagopa/pn/statemachinemanager/configuration/ServerAspectLoggingActivation.java
+++
@@ -1,8 +0,0 @@
-package it.pagopa.pn.statemachinemanager.configuration;
-
-import it.pagopa.pn.commons.utils.ServerAspectLogging;
-import org.springframework.stereotype.Component;
-
-@Component
-public class ServerAspectLoggingActivation extends ServerAspectLogging {
-}
