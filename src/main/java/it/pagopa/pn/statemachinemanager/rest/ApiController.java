e777b7d16f2f0708937250b8083b3334a842277c
mdisco-dgs <marco.disco@dgsspa.com>
2026-09-17 14:57:55 +0200

    PN-21344: activate platform logging aspects (Server/Client)

--- src/main/java/it/pagopa/pn/statemachinemanager/rest/ApiController.java
+++ src/main/java/it/pagopa/pn/statemachinemanager/rest/ApiController.java
@@ -1,20 +1,15 @@
 package it.pagopa.pn.statemachinemanager.rest;
 
-import it.pagopa.pn.statemachinemanager.exception.StateMachineManagerException;
 import it.pagopa.pn.statemachinemanager.generated.openapi.server.v1.api.StateMachineControllerApi;
 import it.pagopa.pn.statemachinemanager.generated.openapi.server.v1.dto.ExternalStatusResponse;
 import it.pagopa.pn.statemachinemanager.generated.openapi.server.v1.dto.ValidateStatusResponse;
 import it.pagopa.pn.statemachinemanager.service.StateMachineService;
-import lombok.CustomLog;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.server.ServerWebExchange;
 import reactor.core.publisher.Mono;
 
-import static it.pagopa.pn.statemachinemanager.constants.Constants.*;
-
 @RestController
-@CustomLog
 public class ApiController implements StateMachineControllerApi {
 
 
@@ -27,21 +22,13 @@ public class ApiController implements StateMachineControllerApi {
 
     @Override
     public Mono<ResponseEntity<ValidateStatusResponse>> validateStatus(String process, String status, String clientId, String nextStatus, final ServerWebExchange exchange) {
-
-        log.logStartingProcess(VALIDATE_STATUS);
         return Mono.fromCallable(() -> service.queryTable(process, status, clientId, nextStatus))
-                .map(ResponseEntity::ok)
-                .doOnSuccess(response -> log.logEndingProcess(VALIDATE_STATUS))
-                .doOnError(StateMachineManagerException.class, exception -> log.logEndingProcess(VALIDATE_STATUS, false, exception.getMessage(), exception));
+                .map(ResponseEntity::ok);
     }
 
     @Override
     public Mono<ResponseEntity<ExternalStatusResponse>> getExternalStatus(String process, String status, String clientId, final ServerWebExchange exchange) {
-
-        log.logStartingProcess(GET_EXTERNAL_STATUS_PROCESS);
         return Mono.fromCallable(() -> service.getExternalStatus(process, status, clientId))
-                .map(ResponseEntity::ok)
-                .doOnSuccess(response -> log.logEndingProcess(GET_EXTERNAL_STATUS))
-                .doOnError(StateMachineManagerException.class, exception -> log.logEndingProcess(GET_EXTERNAL_STATUS, false, exception.getMessage(), exception));
+                .map(ResponseEntity::ok);
     }
 }
